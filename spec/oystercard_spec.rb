0ed372a73de80183c6ca7cf33673ce9be3a5d7a6
Arif Ebrahim <>
2021-05-04 15:09:57 +0100

    added balance instance variable

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,9 @@
-describe Oystercard do
+require 'oystercard'
 
+RSpec.describe Oystercard do
+  describe "#balance" do
+    it "should return the balance on the card" do
+      expect(subject.balance).to eq (0)
+    end
+  end
 end
